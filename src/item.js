eba365a86a648b99a56acce10b14606e15e366df
AndVikVin <vintskevich@gmail.com>
2018-03-05 23:06:26 +0200

    Ready without right css

--- src/item.js
+++ src/item.js
@@ -5,27 +5,34 @@ class Item extends Component{
         super(props)
 
         this.state = {
-           quantity: this.props.it.quantity
-        
+           quantity: this.props.it.quantity,
+           doneQuantity: 0,
+           isDone: false 
         }
         this.done = this.done.bind(this);
         this.delete = this.delete.bind(this);
     }
 
     render(){
+        
         return(
             <section>
-                <p>{this.props.it.name + ' ' + this.state.quantity}</p>
+                <p className = {(this.state.isDone === false)?"undone":"done"}>{this.props.it.name + ' ' + this.state.quantity + '/' + this.state.doneQuantity}</p>
                 <button onClick={this.done}>Done!</button>
                 <button onClick = {this.delete} >Delete</button>
             </section>
         )
     }
     done(){
-        const res = this.state.quantity;
-        this.setState({
-            quantity: +res + 1
-        })
+        const res = this.state.doneQuantity;
+        (res < this.state.quantity)?this.setState({
+            doneQuantity: +res + 1
+        }):true;
+         if(res === +this.state.quantity-1)
+            this.setState({
+                isDone: true
+            });
+        
     }
 
     delete(){
